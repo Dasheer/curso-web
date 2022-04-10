5803fbf9d5d3ccb83263588a00c17d486d1d294d
Daniel <danieleduardo.pc@outlook.com>
2022-04-10 17:13:29 -0500

    Variables con let y const JavaScript

---
+++ proyectos_curso/introduccion_js/js/02.js
@@ -0,0 +1,22 @@
+// Variables con let, viene siendo parecido a var
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible; // iniciamos la variable sin valor
+
+//producto = true; // reasignamos el valor de la variable
+
+disponible = true;
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+// var 1disponible
+let disponible_;
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD';
+let nombreProducto = 'Monitor HD';
+let NombreProducto = 'Monitor HD'
+let nombreproducto = 'Monitor HD';
---
+++ proyectos_curso/introduccion_js/js/03.js
@@ -0,0 +1,22 @@
+// Variables con const, los cuales son pueden cambiar, o inicializar sin valor
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+//const disponible; // iniciamos la variable sin valor
+
+//producto = true; // reasignamos el valor de la variable
+
+//disponible = true;
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+// var 1disponible
+//const disponible_;
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD';
+const nombreProducto = 'Monitor HD';
+const NombreProducto = 'Monitor HD'
+const nombreproducto = 'Monitor HD';
