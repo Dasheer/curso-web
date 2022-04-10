8881f32f20cdb710acbfeee08f32cedaabfe5d62
Daniel <danieleduardo.pc@outlook.com>
2022-04-10 16:46:51 -0500

    Variables en JavaScript

---
+++ proyectos_curso/introduccion_js/js/01.js
@@ -0,0 +1,24 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // iniciamos la variable sin valor
+
+//producto = true; // reasignamos el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadora';
+
+// var 1disponible
+var disponible_;
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD';
+var nombreProducto = 'Monitor HD';
+var NombreProducto = 'Monitor HD'
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
