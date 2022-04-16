e1f46d3fcaa1075e48658cd14be05207a7a3db60
Daniel <danieleduardo.pc@outlook.com>
2022-04-16 13:02:56 -0500

    Números y Operadores

---
+++ proyectos_curso/introduccion_js/js/05.js
@@ -0,0 +1,21 @@
+// Números
+
+const nombre = 'Daniel';
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 7;
+const numero4 = 6;
+const numero5 = -14;
+
+console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero3 % numero4);
