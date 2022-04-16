ab39638500530b7911e1a66d66e24ad61c129add
Daniel <danieleduardo.pc@outlook.com>
2022-04-16 11:48:01 -0500

    Métodos para los Strings

--- proyectos_curso/introduccion_js/js/04.js
+++ proyectos_curso/introduccion_js/js/04.js
@@ -1,4 +1,17 @@
 // String o Cadenas de Texto
-const producto = "Monitor de 20\"";
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo Web Completo";
+const producto2 = "Monitor HD";
+const email = 'correo@correo.com'
 
-console.log(producto);
+// length es para la extension
+console.log(tweet.length);
+console.log(producto2);
+
+// IndexOf para encontrar un elemento en un string
+console.log(tweet.indexOf('JavaScript'));
+console.log(producto2.indexOf('Tablet'));
+console.log(email.includes('@'));
+
+// Includes (retorna true o false)
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('Tablet'));
