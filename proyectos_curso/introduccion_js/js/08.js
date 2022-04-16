edadd574677271c1fec503f2753de1841e47911e
Daniel <danieleduardo.pc@outlook.com>
2022-04-16 13:31:45 -0500

    Concatenación y Template Strings

---
+++ proyectos_curso/introduccion_js/js/08.js
@@ -0,0 +1,8 @@
+const nombre = 'Daniel';
+const email = 'correo@correo.com';
+
+// Concatenación
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+// Template Strings - Strings Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
