df0f506840f6f5294ba550d9d227ef64d5b40f47
Daniel <danieleduardo.pc@outlook.com>
2022-04-17 18:35:28 -0500

    Prototypes

--- proyectos_curso/introduccion_js/js/26.js
+++ proyectos_curso/introduccion_js/js/26.js
@@ -10,4 +10,4 @@ const reservacion = {
     }
 }
 
-reservacion.informacion();
+//reservacion.informacion();
--- proyectos_curso/introduccion_js/js/27.js
+++ proyectos_curso/introduccion_js/js/27.js
@@ -8,6 +8,18 @@ const producto = {
 
 /** Object Constructor **/
 
+function Cliente(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+const cliente = new Cliente('Daniel', 'Pech');
+
+Cliente.prototype.formatearCliente = function () {
+    return `El cliente ${this.nombre} ${this.apellido}`;
+}
+
+
 function Producto(nombre, precio, disponible) {
     this.nombre = nombre;
     this.precio = precio;
@@ -19,8 +31,19 @@ const producto3 = new Producto('Laptop', 300, true);
 const producto4 = new Producto('Celular', 250, true);
 const producto5 = new Producto('CPU', 600, false);
 
+// Crear funciones que solo se utilizan en un objeto en especifico
+Producto.prototype.formatearProducto = function () {
+    return `El producto ${this.nombre} tiene un precio de: ${this.precio}`;
+}
 
-console.log(producto2);
-console.log(producto3);
-console.log(producto4);
-console.log(producto5);
+// function  formatearProducto(producto) {
+//     return `El producto ${producto.nombre} tiene un precio de: ${producto.precio}`;
+// }
+
+console.log(cliente.formatearCliente());
+
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto());
+console.log(producto4.formatearProducto());
+console.log(producto5.formatearProducto());
+
