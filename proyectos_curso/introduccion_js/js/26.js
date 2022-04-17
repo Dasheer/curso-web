c8a454711a21acc485b2da4ea876351fdb15e1c0
Daniel <danieleduardo.pc@outlook.com>
2022-04-17 17:32:08 -0500

    this en JavaScript

---
+++ proyectos_curso/introduccion_js/js/26.js
@@ -0,0 +1,13 @@
+// This
+
+const reservacion = {
+    nombre: 'Daniel',
+    apellido: 'Pech',
+    total: 5000,
+    pagado: false,
+    informacion: function () {
+        console.log(`El cliente ${this.nombre} reservó, y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+reservacion.informacion();
