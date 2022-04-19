a52977a7e3431ff856d12aa3c0fdb41150f92505
Daniel <danieleduardo.pc@outlook.com>
2022-04-19 07:13:33 -0500

    Revert "Revert "Seleccionar Elementos con querySelector""
    This reverts commit 5aa35146abedfca83dfcf652ead1a88fe1999f7f.

---
+++ proyectos_curso/js_dom/js/script.js
@@ -0,0 +1,10 @@
+// querySelector
+const heading = document.querySelector('#heading') // 0 o 1 Elemento
+heading.textContent = 'Nuevo Heading';
+// heading.classList.add('nueva-clase');
+console.log(heading)
+
+// querySelectorAll
+
+
+// getElementById
