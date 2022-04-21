76d85e2015b05b119e6d842107eaee85251fc981
Daniel <danieleduardo.pc@outlook.com>
2022-04-21 17:51:06 -0500

    Arreglos asociativos o Associative Arrays en PHP

--- proyectos_curso/introduccion_php/09-arrays-assoc.php
+++ proyectos_curso/introduccion_php/09-arrays-assoc.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Daniel',
+    'saldo'  =>  200,
+    'informacion' => [
+        'tipo' => 'Premium',
+        'disponible' => 100
+    ]
+];
 
+echo '<pre>';
+var_dump($cliente['nombre']);
+echo '<pre>';
+
+echo $cliente['nombre'];
+
+//echo $cliente['informacion']['tipo'];
+
+$cliente['codigo'] = 1233454;
+echo '<pre>';
+var_dump($cliente);
+echo '<pre>';
 
 
 include 'includes/footer.php';
