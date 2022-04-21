eb80757f6f19265e5c33cf19b735eb8275744fc4
Daniel <danieleduardo.pc@outlook.com>
2022-04-21 14:44:52 -0500

    Variables y Constantes en PHP

--- proyectos_curso/introduccion_php/02-variables.php
+++ proyectos_curso/introduccion_php/02-variables.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
+$nombre = 'Daniel';
 
+$nombre = 'Eduardo';
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', 'EFE');
+echo constante;
+
+const conse = 'F';
+echo conse;
+
+$nombreCliente = 'Andrea';
+$nombre_cliente = 'Andrea;';
 
 
 include 'includes/footer.php';
