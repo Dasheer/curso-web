ad05706766bab03a021f50a704adeddd026f959b
Daniel <danieleduardo.pc@outlook.com>
2022-04-21 19:59:02 -0500

    if, else if y switch

--- proyectos_curso/introduccion_php/12-condicionales.php
+++ proyectos_curso/introduccion_php/12-condicionales.php
@@ -1,7 +1,56 @@
 <?php include 'includes/header.php';
 
+$autenticado = true;
+$admin = false;
 
+if ($autenticado && $admin) {
+    echo 'Usuario autendicado correctamente';
+} else {
+    echo 'Usuario no autenticado, inicia sesión';
+}
 
+// If anidados
+$cliente = [
+    'nombre' => 'Daniel',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
 
+echo '<br>';
+
+if(!empty($cliente)) {
+    echo 'El arreglo de cliente no está vacío';
+    if ($cliente['saldo']  > 0) {
+        echo 'Saldo disponible';
+    } else {
+        echo 'Saldo insuficiente';
+    }
+}
+
+// else if
+echo '<br>';
+
+if ($cliente['saldo'] > 0) {
+    echo 'Saldo disponible';
+} else if ($cliente['informacion']['tipo'] === 'Premium') {
+    echo 'Premium';
+} else {
+    echo 'sin datos';
+}
+
+// swtich
+echo '<br>';
+$tecnologia = 'PHP';
+
+switch ($tecnologia) {
+    case 'PHP':
+        echo "PHP, un excelente lenguaje";
+        break;
+    default:
+        echo 'Algun lenguaje';
+        break;
+}
 
 include 'includes/footer.php';
