5b00f3a3ed18de6d403ac1349738edcd0fee8ffe
Daniel <danieleduardo.pc@outlook.com>
2022-04-22 22:15:25 -0500

    Utilizando Gulp para crear imágenes Webp

--- proyectos_curso/festival_musica/gulpfile.js
+++ proyectos_curso/festival_musica/gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Imágenes
+const webp = require('gulp-webp');
 
 function css(done) {
     src('src/scss/**/*.scss')     // Identificar el archivo .SCSS a compilar
@@ -10,10 +15,23 @@ function css(done) {
     done();
 }
 
+function vrWebp (done) {
+
+    const opc = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opc))
+        .pipe(dest('build/img'))
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.vrWebp = vrWebp;
+exports.dev = parallel(vrWebp,dev);
