c3aef18bd04882244fefd240832c1fd8eda4fd6f
Daniel <danieleduardo.pc@outlook.com>
2022-04-23 16:03:55 -0500

    Añadir un botón para cerrar la imagen

--- proyectos_curso/festival_musica/build/js/app.js
+++ proyectos_curso/festival_musica/build/js/app.js
@@ -33,10 +33,30 @@ function showImagen(id) {
             <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
     `;
 
+    // Crea el Overlay con la imagen
     const overlay = document.createElement('DIV');
     overlay.appendChild(imagen);
     overlay.classList.add('overlay');
+    overlay.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
 
+    // Boton para cerrar el modal
+    const closeModal = document.createElement('P');
+    closeModal.textContent = 'X';
+    closeModal.classList.add('btn-cerrar');
+    closeModal.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(closeModal);
+
+    // Añadirlo al HTML
     const body = document.querySelector('body');
     body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
 }
