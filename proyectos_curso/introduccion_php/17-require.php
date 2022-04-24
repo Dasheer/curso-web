abbb7f2cafc902d4cee089d71648b7b81831fc64
Daniel <danieleduardo.pc@outlook.com>
2022-04-24 08:27:40 -0500

    Include y require

--- proyectos_curso/introduccion_php/17-require.php
+++ proyectos_curso/introduccion_php/17-require.php
@@ -1,8 +1,17 @@
 <?php include 'includes/header.php';
 
+require 'funciones.php';
 
+initApp();
 
-
-
+/**
+ * include() dependiendo de la configuración si no encuentra el archivo
+ * sigue ejecutandose
+ *
+ * requiere() si no encuentra el archivo deja de ejecutar todo
+ *
+ * requiere_one() es exactamente igual con la diferencia que si el archivo
+ * ya fue incluido, lo ignora, si no ha sido incluido, lo va a incluir
+ **/
 
 include 'includes/footer.php';
