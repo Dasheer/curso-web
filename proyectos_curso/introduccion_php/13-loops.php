a14f12c782994886c6572fd19cbe98178812c631
Daniel <danieleduardo.pc@outlook.com>
2022-04-23 22:17:42 -0500

    For loops

--- proyectos_curso/introduccion_php/13-loops.php
+++ proyectos_curso/introduccion_php/13-loops.php
@@ -1,6 +1,35 @@
 <?php include 'includes/header.php';
 
+// while
+$i = 100;
+while ($i < 10) {
+    echo $i . '<br>';
 
+    $i++;
+}
 
+// Do While
+$i = 100;
+do {
+    echo $i . '<br>';
+    $i++;
+} while ($i < 10);
+
+/**
+ * 3 imprimir Fizz
+ * 5 imprimir Buzz
+ * 3 y 5 imprimir Fizz Buzz
+ **/
+
+// For Loop
+for ($i = 1; $i < 1000; $i++) {
+    if ($i%3===0 && $i%5===0) {
+        echo $i . ' - FIZZ BUZZ <br>';
+    } else if ($i%3===0) {
+        echo $i . ' - Fizz <br>';
+    } else if ($i%5===0) {
+        echo $i . ' - Buzz <br>';
+    }
+}
 
 include 'includes/footer.php';
