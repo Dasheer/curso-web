d7c32e83f900b945e125b63890f7dcc96c51f899
Daniel <danieleduardo.pc@outlook.com>
2022-04-24 07:40:33 -0500

    while, do while y foreach

--- proyectos_curso/introduccion_php/13-loops.php
+++ proyectos_curso/introduccion_php/13-loops.php
@@ -22,14 +22,31 @@ do {
  **/
 
 // For Loop
-for ($i = 1; $i < 1000; $i++) {
-    if ($i%3===0 && $i%5===0) {
-        echo $i . ' - FIZZ BUZZ <br>';
-    } else if ($i%3===0) {
-        echo $i . ' - Fizz <br>';
-    } else if ($i%5===0) {
-        echo $i . ' - Buzz <br>';
-    }
+//for ($i = 1; $i < 1000; $i++) {
+//    if ($i%3===0 && $i%5===0) {
+//        echo $i . ' - FIZZ BUZZ <br>';
+//    } else if ($i%3===0) {
+//        echo $i . ' - Fizz <br>';
+//    } else if ($i%5===0) {
+//        echo $i . ' - Buzz <br>';
+//    }
+//}
+
+// For Each
+$clientes = array('Pedro', 'Juan', 'Karen');
+
+foreach ($clientes as $cliente) {
+    echo $cliente . '<br>';
+}
+
+$cliente = [
+  'nombre' => 'Daniel',
+  'saldo' => 200,
+  'tipo' => 'Premium'
+];
+
+foreach ($cliente as $key => $valor) {
+    echo $key . ' - ' . $valor . '<br>';
 }
 
 include 'includes/footer.php';
